8daecda947feb9d9a6c9226a61c8bd08dc3b2dc8
Feldsparse <charles.motta@ucas-edu.net>
2026-09-22 09:28:45 -0600

    ethwe

--- KM-CP1-Projects-main/KM-CP1-Projects-main/projects/crew_shares.py
+++ KM-CP1-Projects-main/KM-CP1-Projects-main/projects/crew_shares.py
@@ -3,7 +3,7 @@ import random
 total = random.randint(500, 5000)
 while True:
     try:
-        crew = int(input("How big is the crew ? "))
+        crew = int(input("How big is the crew not counting peter and yondu ? "))
     except:
        print("sorry but that's not valid number")
     else:
@@ -13,9 +13,9 @@ total = total - crew * 3
 yondus_share = total * 0.13
 yondus_share = round(yondus_share, 2)
 total = total - yondus_share
-peters_share = total * 0.13
+peters_share = total * 0.11
 peters_share = round(peters_share, 2)
-total = total - yondus_share
+total = total - peters_share
 crew_share = total / crew
 crew_share = round(crew_share, 2)
 yondus_share = yondus_share + crew_share
@@ -23,3 +23,4 @@ peters_share = peters_share + crew_share
 print("looks like yondu pulled a fast one and pocketed some extra and ended up with " + str(yondus_share))
 print("Nepotism wins again as yondu gave peter some extra with him ending up with " + str(peters_share))
 print("this means each crew member only ended up with " + str(crew_share))
+#i fixed the mistakes
