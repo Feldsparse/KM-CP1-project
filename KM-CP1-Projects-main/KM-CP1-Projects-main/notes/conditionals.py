ff691950dca83e9f6ffdf75853909f5eb3abe3b1
Feldsparse <charles.motta@ucas-edu.net>
2026-09-24 09:52:48 -0600

    jtrsj

---
+++ KM-CP1-Projects-main/KM-CP1-Projects-main/notes/conditionals.py
@@ -0,0 +1,18 @@
+#katia motta: conditionals
+grade = 64
+
+if grade >= 90:
+    print("you have an A! Good job")
+elif grade >= 70:
+    print("you are passing")
+else:
+    print("you are not passing!")
+    print("do you need to retake a quiz? or do you need to turn in a missing assignment")
+
+
+raining = False
+
+if raining:
+    print("bring an umbrella")
+else:
+    print("Wear sunscreen")
---
+++ KM-CP1-Projects-main/KM-CP1-Projects-main/projects/user_signin.py
@@ -0,0 +1,9 @@
+#charles motta: user sign in
+
+username = "kyle"
+password =  "kylerulz"
+
+if input("what is your username? ") != username:
+    print("Incorrect username")
+elif input("what is your password? ") != password:
+    
